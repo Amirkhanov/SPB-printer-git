35f48ee92637a70d811b8569b657f98a5c8dbed3
nikelamigo@gmail.com <nikelamigo@gmail.com>
2018-02-14 11:23:21 +0500

    Страница с галереей

--- dist/images/scripts/main.js
+++ dist/images/scripts/main.js
@@ -407,5 +407,27 @@ $(document).ready(function() {
     })();
 
 
+    $(function() {
+        function galerryPreviuw(image) {
+            var bigPic = $('.gallery-block__left .big-pic')
+
+            bigPic.css('background-image', 'url(' + image + ')');
+        }
+        var href = $('.gallery-block__left .pic-list a.active').attr('href');
+
+        galerryPreviuw(href);
+
+        $('.gallery-block__left .pic-list a').on('click', function(e) {
+            e.preventDefault();
+
+            var $this = $(this),
+                href = $this.attr('href');
+
+            $('.gallery-block__left .pic-list a').removeClass('active');
+            $this.addClass('active');
+
+            galerryPreviuw(href);
+        });
+    });
 
 });
